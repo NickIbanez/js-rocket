35f4710e7886adee3060df473e9785cbbaf5542d
NickIbanez <65280590+NickIbanez@users.noreply.github.com>
2020-05-15 11:40:48 +0200

    Made the rocket fly

--- JS/rocket.js
+++ JS/rocket.js
@@ -15,6 +15,20 @@ var changeState = function (state) {
 		document.getElementById('countdown').
 			innerHTML = countdownNumber;
 		
+		if (countdownNumber > 4 && countdownNumber >= 7) {
+			// Be nervous
+			document.getElementById('nervous').className = 'nervous show'
+		} else	{
+			document.getElementById('nervous').className = 'nervous'
+		}
+
+		if (countdownNumber <= 6 && countdownNumber >= 4) {
+			// cant wait
+			document.getElementById('cant-wait').className = 'cant-wait show'
+		} else {
+			document.getElementById('cant-wait').className = 'cant-wait'
+		}
+
 		if (countdownNumber <= 0) {
 			changeState(3);
 		};
@@ -26,7 +40,7 @@ var changeState = function (state) {
 
 			console.log('randomNumber', randomNumber)
 
-			if (randomNumber > 5) {
+			if (randomNumber > 9) {
 				changeState(4);
 			} else {
 				changeState(5); // failure
